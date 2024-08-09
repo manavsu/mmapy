37efabf6ec4bf49c1c2f81b0baef6cedf72839d6
Manav Singh <manavjotsingh17@gmail.com>
2024-08-09 09:38:22 -0700

    added syn_scan

--- arp_scan.py
+++ arp_scan.py
@@ -3,7 +3,7 @@ logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 log = logging.getLogger("mmapy")
 
-from scapy.all import ARP, ICMP, IP, sr, sr1, Ether, srp, conf
+from scapy.all import ARP, ICMP, IP, TCP, sr, sr1, Ether, srp, conf
 import time
 import utils
 import ipaddress
@@ -74,51 +74,40 @@ def ip_on_local_network(ip:str):
         
     network = local_network()
     return all(ip in network for ip in lan.hosts())
-
-
-def ping_scan(ip_range:str, packet_timeout:int=1):
+    
+def syn_scan(host, port=range(0,10000), timeout=1):
     """
-    Perform a ping scan on the given IP range. May not report all available hosts.
+    Perform a SYN scan on the specified host and port range.
 
     Args:
-        ip_range (str): The IP range to scan in CIDR notation.
-        packet_timeout (int, optional): The timeout for each packet sent.
+        host (str): The target host to scan.
+        port (range, optional): The range of ports to scan. Defaults to range(0,10000).
+        timeout (int, optional): The timeout value for each scan request. Defaults to 1.
 
     Returns:
-        list[str]: A list of discovered host ips.
-        
-    Raises:
-        ValueError: If the IP range is not on the local network.
+        list: A list of tuples containing the open ports found during the scan. Each tuple contains the source IP address and the corresponding open port.
+
     """
-    # utils.check_root_access()
-    validate_ip(ip_range)
-    live_hosts = []
-
-    # Perform the ping scan
-    for ip in IP(dst=ip_range):
-        print(ip)
-        pkt = IP(dst=str(ip))/ICMP()
-        reply = sr1(pkt, timeout=1)
-        if reply:
-            print(f"{ip} is up")
-        else:
-            print(f"{ip} is down")
-    # pkt = IP(dst=ip_range)/ICMP()
-    # print(pkt)
-    # reply = sr(pkt, timeout=packet_timeout)
-    # print(reply)
-
-    # return live_hosts
+    packet = IP(dst=host)/TCP(dport=port, flags="S")
+    packet = list(packet.__iter__())
+    open_ports = []
+    start_time = time.time()
+    for p in range(0, len(packet), 100):
+        response, _ = sr(packet[p:p+100], timeout=timeout, verbose=0)
+        for _, received in response:
+            if received.haslayer(TCP):
+                if received[TCP].flags == "SA":
+                    open_ports.append((received.src, received[TCP].sport))
+    log.debug(f"{host} scanned {syn_scan.__name__} in {time.time() - start_time:.2f} seconds.")
+    return open_ports
     
-    
-    
-def arp_scan(ip_range, packet_timeout:int=1):
+def arp_scan(ip_range, timeout=1):
     """
     Perform an ARP scan on the given IP range.
 
     Args:
         ip_range (str): The IP range to scan in CIDR notation.
-        packet_timeout (int, optional): The timeout for each packet sent.
+        packet_timeout (int, optional): The timeout for each packet sent. The default is 1.
 
     Returns:
         list[str]: A list of discovered host ips.
@@ -141,7 +130,7 @@ def arp_scan(ip_range, packet_timeout:int=1):
     result = srp(packet, timeout=1, verbose=False)[0]
     end_time = time.time()
 
-    log.info(f"{ip_range} scanned in {end_time - start_time:.2f} seconds.")
+    log.debug(f"{ip_range} scanned {arp_scan.__name__} in {end_time - start_time:.2f} seconds.")
 
     return [received.psrc for _, received in result]
 
--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ import logging
 log = logging.getLogger("mmapy")
 def check_root_access():    
     if os.geteuid() != 0:
-        log.info("This process is not running as root. Re-executing with sudo...")
+        log.warn("This process is not running as root. Re-executing with sudo...")
         os.execvp("sudo", ["sudo", "python"] + sys.argv)
         
